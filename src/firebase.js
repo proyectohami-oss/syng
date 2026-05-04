2ebadd94758feab4d95396077665809718073e9e
Hamilton Efrain Arroyo Coutiño <efrain@192.168.1.9>
2026-05-03 22:13:57 -0600

    feat: modo offline con IndexedDb persistence

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyC8Ym6HJBerYK5ke6lCiRM32mx7ei2Dn78",
@@ -15,3 +15,4 @@ const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
 export const db = getFirestore(app)
+enableIndexedDbPersistence(db).catch(() => {})
