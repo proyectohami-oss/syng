760a16d1980471ac5050c555e951345d3b7a132f
Hamilton Efrain Arroyo Coutiño <efrain@192.168.1.38>
2026-05-22 15:49:54 -0600

    fix: registerType autoUpdate para que el SW se actualice solo en móvil

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate',
       strategies:   'generateSW',
       filename:     'sw.js',
 
