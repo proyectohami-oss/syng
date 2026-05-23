73919bc61fb7fbf57079d64fe77e2f43e3f4a6cb
Hamilton Efrain Arroyo Coutiño <efrain@192.168.1.38>
2026-05-23 08:29:06 -0600

    fix: skipWaiting en SW para actualizaciones automáticas en iOS

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,8 @@
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching'
 
+self.skipWaiting()
+self.clients.claim()
+
 cleanupOutdatedCaches()
 precacheAndRoute(self.__WB_MANIFEST)
 
